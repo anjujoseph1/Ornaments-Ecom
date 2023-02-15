7662ceee690d57c7cc42268015c84eebe021ee2b
anjujoseph1 <anjuzz955@gmail.com>
2023-02-15 23:36:50 +0530

    2nd part

--- cart_app/urls.py
+++ cart_app/urls.py
@@ -4,4 +4,6 @@ from.import views
 urlpatterns=[
     path('cartt/',views.cartprodt,name='cartdet'),
     path('add/<int:product_id>/',views.addcart,name='addcart'),
+    path('dicrement/<int:prodt_id>/',views.dicrement,name='dicrement'),
+    path('delete/<int:pt_id>/',views.delete,name='delete'),
 ]
--- cart_app/views.py
+++ cart_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from ornaments_app.models import *
 from django.core.exceptions import ObjectDoesNotExist
 from.models import *
@@ -6,8 +6,8 @@ from.models import *
 
 def cartprodt(request,tot=0,count=0,carti=None):
     try:
-        cartl=CartList.objects.get(cart_id=c_id(requrement))
-        carti=CartItems.objects.filiter(cartli=cartl)
+        cartl=CartList.objects.get(cart_id=c_id(request))
+        carti=CartItems.objects.filter(cartli=cartl)
         for i in carti:
             tot += (i.prot.price * i.quantity)
             count += i.quantity
@@ -29,12 +29,29 @@ def addcart(request,product_id):
         car=CartList.objects.create(cart_id=c_id(request))
         car.save()
     try:
-        cartte=CartItems.objects.get(produ=prot,ct=cartli)
+        cartte=CartItems.objects.get(prot=prodall,cartli=car)
         if cartte.quantity<cartte.prot.stock:
             cartte.quantity += 1
             cartte.save()
     except CartItems.DoesNotExist:
-        cartte=CartItems.objects.create(prodd=prot,quantity=1,ct=cartli)
+        cartte=CartItems.objects.create(prot=prodall,quantity=1,cartli=car)
         cartte.save()
     return redirect('cartdet')    
             
+def dicrement(request,prodt_id):
+    cart=CartList.objects.get(cartd=c_id(request))
+    product=get_object_or_404(Products,id=prodt_id)
+    cartem=CartItems.objects.get(prot=product,cartli=cart)
+    if cartem.quantity >1:
+        cartem.quantity -=1
+        cartem.save()
+    else:
+        cartem.delete()
+    return redirect('cartdet')        
+
+def delete(request,pt_id):
+    cat=CartList.objects.get(cartd=c_id(request))
+    pt=get_object_or_404(Products,id=pt_id)
+    cartm=CartItems.objects.get(prot=pt,cartli=cat)
+    cartm.delete()
+    return redirect('cartdet')
